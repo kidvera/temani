05f66c9b5ef1c638d7a9ce045b13360d85bd4ab6
kidvera <kidvera.id@gmail.com>
2026-05-13 14:02:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ═══════════════════════════════════════════
 // CONFIG — GANTI DENGAN NILAI KAMU
 // ═══════════════════════════════════════════
-const SUPABASE_URL = 'https://sb_publishable__4QG6YPTKjHGOUaNJSNdYg_7aI3UC8-';
+const SUPABASE_URL = 'https://spflyyqvawiiuazchyht.supabase.co/rest/v1/';
 const SUPABASE_ANON_KEY = 'sb_secret_yAJ0blCdDAolXDeGEbNneA_U1Z7zwfO';
 const LYNK_URL = 'http://lynk.id/r4hm4wati/18d2eg4nl2gy/checkout'; // ganti dengan link Lynk.id kamu
 
